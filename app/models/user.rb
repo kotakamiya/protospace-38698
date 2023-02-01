5eea063eacc6d99a75b097b8aaab30c5511325b6
kotakamiya <118578397+kotakamiya@users.noreply.github.com>
2023-02-01 11:41:34 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  validates :name, presence: true
-  validates :profile, presence: true
-  validates :occupation, presence: true
-  validates :position, presence: true
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: '全角カタカナを使用してください' } do
+    validates :first_kana
+    validates :last_kana
+  end  
+  validates :birth_day, presence: true
 
   has_many :prototypes, dependent: :destroy
   has_many :comments
